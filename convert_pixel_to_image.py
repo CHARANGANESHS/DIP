2eae9690d0098d6708db13f7beee4e737d1e3812
CHARANGANESHS <charanganesh10@gmail.com>
2024-06-08 00:55:10 +0530

    Added the alphabet map

--- convert_pixel_to_image.py
+++ convert_pixel_to_image.py
@@ -35,10 +35,41 @@ def foo():
         
         # Create a new file named output_images_csv, which has first column as the name of the image and second column as the pixel label i.e.., csv_data.iloc[i][0]
         
+        # Create a map for the alphabets and numbers
+        alphabet_map = {
+            0: 'A',
+            1: 'B',
+            2: 'C',
+            3: 'D',
+            4: 'E',
+            5: 'F',
+            6: 'G',
+            7: 'H',
+            8: 'I',
+            9: 'J',
+            10: 'K',
+            11: 'L',
+            12: 'M',
+            13: 'N',
+            14: 'O',
+            15: 'P',
+            16: 'Q',
+            17: 'R',
+            18: 'S',
+            19: 'T',
+            20: 'U',
+            21: 'V',
+            22: 'W',
+            23: 'X',
+            24: 'Y',
+            25: 'Z',
+        }
+        
+        
         output_images_csv = 'output_images_csv.csv'
         image_name =  str(i+1) + '.png'
         image_label = csv_data.iloc[i][0]
-        image_info = pd.DataFrame({'Image Name': image_name, 'Image Label': image_label})
+        image_info = pd.DataFrame({'Image Name': image_name, 'Image Label': alphabet_map[image_label]})
         image_info.to_csv(output_images_csv, index=False, mode='a', header=not os.path.exists(output_images_csv))
         
         
